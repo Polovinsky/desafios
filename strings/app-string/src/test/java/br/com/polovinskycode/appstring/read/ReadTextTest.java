f6401b75a2392328d0b0713b77e528ef545d441b
Diego Rocha <diegopolovinsky@gmail.com>
2018-05-05 19:30:30 -0300

    refactor method break words

--- strings/app-string/src/test/java/br/com/polovinskycode/appstring/read/ReadTextTest.java
+++ strings/app-string/src/test/java/br/com/polovinskycode/appstring/read/ReadTextTest.java
@@ -1,15 +1,25 @@
 package br.com.polovinskycode.appstring.read;
 
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+
 public class ReadTextTest {
+
+    private ReadText readText;
 
     @Test
     public void read_withThreeWords_shouldReturnArray() throws Exception {
         String text = "Diego Rocha Ramos";
-        ReadText read = new ReadText(0, text);
-        Assert.assertThat(read.breakWords().length, Matchers.equalTo(3));
+        readText = new ReadText(0, text);
+        assertThat(readText.breakWords().length, Matchers.equalTo(3));
+    }
+
+    @Test
+    public void read_withThreeWords_shouldReturn_xxx() throws Exception {
+        String text = "Diego Rocha Ramos";
+        readText = new ReadText(0, text);
+        assertThat(readText.breakWords().length, Matchers.equalTo(3));
     }
 }
